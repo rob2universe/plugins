5246f8574e89163b4eaa602c66f89bf5c4d8c86a
rob2universe <robert.emsbach@gmail.com>
2021-03-03 18:06:05 +0800

    Added properties plugin

---
+++ camunda-modeler-property-info-plugin/README.md
@@ -0,0 +1,53 @@
+<h1 align="center">
+  Camunda Modeler Property Info Plugin
+</h1>
+
+<h4 align="center">This plugin serves a better overview which properties are set per shape.</h4>
+
+<p align="center">
+    <img src="https://img.shields.io/badge/version-0.0.1-brightgreen.svg"
+         alt="version 0.0.1">
+  <a href="https://github.com/camunda/camunda-modeler"><img src="https://img.shields.io/badge/modeler-1.8.2-brightgreen.svg"
+         alt="version 1.8.2"></a>
+  <a href="http://www.apache.org/licenses/"><img src="https://img.shields.io/badge/license-Apache_2.0-blue.svg"
+         alt="license Apache 2.0"></a>
+  <a href="http://www.umb.ch"><img src="https://img.shields.io/badge/with_&#10084; from-UMB-yellow.svg?colorB=FED000"
+         alt="with love from umb"></a>
+</p>
+<br>
+
+<div align="center">
+<img src="./plugin-example.png"
+         alt="plugin example">
+</div>
+
+## How to install
+
+Adding a plugin is as easy as putting the directory into the directory `{MODELER_LOCATION}/plugins`.
+On macOS you have to create the `plugins` folder in this directoy: `/Users/{USER_NAME}/Library/Application Support/camunda-modeler`. A compressed version is always available in the release section.
+
+## Possible Badges
+
+![I](./docs/badges/I.png) Input<br/>
+![O](./docs/badges/O.png) Output<br/>
+![V](./docs/badges/V.png) Variable
+
+![L](./docs/badges/L.png) Listener<br/>
+![T](./docs/badges/T.png) Task Listener
+
+![E](./docs/badges/E.png) Extension<br/>
+![F](./docs/badges/F.png) Field Injection
+
+![E](./docs/badges/Play.png) Is Executable<br/>
+![F](./docs/badges/Pause.png) Is not Executable
+## Issues & ideas
+
+:bulb: Save toggle preferences in modeler<br/>
+:bulb: Add implementation for cmmn<br/>
+:bulb: Show toggle state<br/>
+:bulb: Add implementation for sequence flows
+
+
+## Licence
+
+[Apache License, Version 2.0, January 2004](http://www.apache.org/licenses/)
---
+++ camunda-modeler-property-info-plugin/client/PropertyInfoPlugin.js
@@ -0,0 +1,327 @@
+'use strict';
+
+var _ = require('lodash');
+
+var elementOverlays = [];
+var overlaysVisible = true;
+
+function PropertyInfoPlugin(eventBus, overlays, elementRegistry, editorActions) {
+
+    eventBus.on('shape.changed', function (event) {
+        _.defer(function () {
+            changeShape(event);
+        });
+    });
+
+    eventBus.on('shape.removed', function (event) {
+        var element = event.element;
+
+        _.defer(function () {
+            removeShape(element);
+        });
+    });
+
+    eventBus.on('shape.added', function (event) {
+        _.defer(function () {
+            changeShape(event);
+        });
+    });
+
+
+    editorActions.register({
+        togglePropertyOverlays: function () {
+            toggleOverlays();
+        }
+    });
+
+    function changeShape(event) {
+        var element = event.element;
+        if (!(element.businessObject.$instanceOf('bpmn:FlowNode') || element.businessObject.$instanceOf('bpmn:Participant'))) {
+            return;
+        }
+        _.defer(function () {
+            addStyle(element);
+        });
+    }
+
+    function removeShape(element) {
+        var elementObject = elementOverlays[element.id];
+        for (var overlay in elementObject) {
+            overlays.remove(elementObject[overlay]);
+        }
+        delete elementOverlays[element.id];
+    }
+
+    function toggleOverlays() {
+        if (overlaysVisible) {
+            overlaysVisible = false;
+            if (elementOverlays !== undefined) {
+                for (var elementCount in elementOverlays) {
+                    var elementObject = elementOverlays[elementCount];
+                    for (var overlay in elementObject) {
+                        overlays.remove(elementObject[overlay]);
+                    }
+                }
+            }
+        } else {
+            overlaysVisible = true;
+            var elements = elementRegistry.getAll();
+            for (var elementCount in elements) {
+                var elementObject = elements[elementCount];
+                if (elementObject.businessObject.$instanceOf('bpmn:FlowNode') || elementObject.businessObject.$instanceOf('bpmn:Participant')) {
+                    addStyle(elementObject);
+                }
+            }
+        }
+    }
+
+    function addStyle(element) {
+
+        if (elementOverlays[element.id] !== undefined && elementOverlays[element.id].length !== 0) {
+            for (var overlay in elementOverlays[element.id]) {
+                overlays.remove(elementOverlays[element.id][overlay]);
+            }
+        }
+
+        elementOverlays[element.id] = [];
+
+        if( element.businessObject.documentation !== undefined &&
+            element.businessObject.documentation.length > 0 &&
+            element.businessObject.documentation[0].text.trim() !== "" &&
+            element.type !== "label"){
+
+            var text = element.businessObject.documentation[0].text;
+            text = text.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
+
+            elementOverlays[element.id].push(
+            overlays.add(element, 'badge', {
+                position: {
+                    top: 4,
+                    right: 4
+                },
+                html: '<div class="doc-val-true" data-badge="D"></div><div class="doc-val-hover" data-badge="D">'+text+'</div>'
+            }));
+        }
+
+        if (element.businessObject.extensionElements === undefined && element.businessObject.$instanceOf('bpmn:FlowNode')) {
+            return;
+        }
+
+        //Do not process the label of an element
+        if (element.type === "label") {
+            return;
+        }
+
+        if (!overlaysVisible) {
+            return;
+        }
+
+        var badges = [];
+
+        if(element.businessObject.$instanceOf('bpmn:Participant')) {
+            var extensionElements = element.businessObject.processRef.extensionElements;
+            var extensions = (extensionElements === undefined ? [] : extensionElements.values);
+
+            var type = '&#9654;';
+            var background = 'badge-green';
+            if(element.businessObject.processRef.isExecutable === false) {
+                type = '&#10074;&#10074;';
+                background = 'badge-red';
+            }
+            badges.push({
+                badgeKey: 'isExecutable',
+                badgeSort: 0,
+                badgeType: type,
+                badgeBackground: background,
+                badgeLocation: 'left'
+            });
+        }
+        else {
+            var extensions = element.businessObject.extensionElements.values;
+        }
+
+
+        for (var extension in extensions) {
+            var type = '';
+            var background = '';
+            var location = 'right';
+            var sort = 0;
+            var key = '';
+
+            switch (extensions[extension].$type) {
+                case 'camunda:ExecutionListener':
+                    if (extensions[extension].event === 'start') {
+                        location = 'left';
+                        key = 'camunda:ExecutionListener-start';
+                        sort = 20;
+                    } else {
+                        location = 'right';
+                        key = 'camunda:ExecutionListener-end';
+                        sort = 70;
+                    }
+                    type = 'L';
+                    background = 'badge-green';
+                    break;
+                case 'camunda:Properties':
+                    key = 'camunda:Properties';
+                    sort = 80;
+                    type = 'E';
+                    background = 'badge-violet';
+                    break;
+                case 'camunda:TaskListener':
+                    if (extensions[extension].event === 'create' || extensions[extension].event === 'assignment') {
+                        location = 'left';
+                        key = 'camunda:TaskListener-start';
+                        sort = 21;
+                    } else {
+                        location = 'right';
+                        key = 'camunda:TaskListener-end';
+                        sort = 71;
+                    }
+                    type = 'T';
+                    background = 'badge-green';
+                    break;
+                case 'camunda:InputOutput':
+                    background = 'badge-blue';
+                    break;
+                case 'camunda:In':
+                    type = 'V';
+                    key = 'camunda:In';
+                    location = 'left';
+                    sort = 10;
+                    background = 'badge-blue';
+                    break;
+                case 'camunda:Out':
+                    type = 'V';
+                    key = 'camunda:Out';
+                    location = 'right';
+                    sort = 60;
+                    background = 'badge-blue';
+                    break;
+                case 'camunda:Field':
+                    key = 'camunda:Field';
+                    sort = 90;
+                    type = 'F';
+                    background = 'badge-red';
+                    break;
+            }
+
+            if (extensions[extension].$type === 'camunda:InputOutput') {
+                if (extensions[extension].hasOwnProperty('inputParameters') &&
+                    extensions[extension].inputParameters.length > 0) {
+                    location = 'left';
+                    type = 'I';
+                    key = 'camunda:InputOutput-input';
+                    sort = 10;
+
+                    badges.push({
+                        badgeKey: key,
+                        badgeSort: sort,
+                        badgeType: type,
+                        badgeBackground: background,
+                        badgeLocation: location
+                    });
+                }
+
+                if (extensions[extension].hasOwnProperty('outputParameters') &&
+                    extensions[extension].outputParameters.length > 0) {
+                    location = 'right';
+                    type = 'O';
+                    key = 'camunda:InputOutput-output';
+                    sort = 60;
+
+                    badges.push({
+                        badgeKey: key,
+                        badgeSort: sort,
+                        badgeType: type,
+                        badgeBackground: background,
+                        badgeLocation: location
+                    });
+                }
+            } else {
+                if (key !== '') {
+                    badges.push({
+                        badgeKey: key,
+                        badgeSort: sort,
+                        badgeType: type,
+                        badgeBackground: background,
+                        badgeLocation: location
+                    });
+                }
+            }
+
+
+        }
+
+        addOverlays(badges, element);
+    }
+
+    function addOverlays(badgeList, element) {
+        var badges = [];
+
+        var leftCounter = 0;
+        var rightCounter = 0;
+
+
+        var sortedBadgeList = uniqBy(badgeList, function (item) {
+            return item.badgeKey
+        });
+
+        sortedBadgeList.sort(function (a, b) {
+            return a.badgeSort - b.badgeSort;
+        });
+
+        for (var overlayCounter in sortedBadgeList) {
+            var overlayObject = sortedBadgeList[overlayCounter];
+
+            if (overlayObject.badgeLocation === 'left') {
+                badges.push(overlays.add(element, 'badge', {
+                    position: {
+                        bottom: 0,
+                        left: leftCounter
+                    },
+                    html: '<div class="badge ' + overlayObject.badgeBackground + '" data-badge="' + overlayObject.badgeType + '"></div>'
+                }));
+                leftCounter = leftCounter + 16;
+            } else {
+                badges.push(overlays.add(element, 'badge', {
+                    position: {
+                        bottom: 0,
+                        right: rightCounter
+                    },
+                    html: '<div class="badge ' + overlayObject.badgeBackground + '" data-badge="' + overlayObject.badgeType + '"></div>'
+                }));
+                rightCounter = rightCounter + 16;
+            }
+
+        }
+
+        pushArray(elementOverlays[element.id],badges);
+    }
+
+    function uniqBy(a, key) {
+        var seen = {};
+        return a.filter(function (item) {
+            var k = key(item);
+            return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+        })
+    }
+
+    function pushArray(list, other) {
+        var len = other.length;
+        var start = list.length;
+        list.length = start + len;
+        for (var i = 0; i < len; i++ , start++) {
+            list[start] = other[i];
+        }
+    }
+
+}
+
+PropertyInfoPlugin.$inject = ['eventBus', 'overlays', 'elementRegistry', 'editorActions'];
+
+module.exports = {
+    __init__: ['clientPlugin'],
+    clientPlugin: ['type', PropertyInfoPlugin]
+};
